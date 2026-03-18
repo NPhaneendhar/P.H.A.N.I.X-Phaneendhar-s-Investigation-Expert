00113c7c4362e66a7f6e4d8c8b819a3c16590f21
phaneendharnittala <nittalaphaneendhar@gmail.com>
2026-03-18 19:24:52 +0530

    completed the error fix

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,15 @@
-const CACHE_NAME = 'phanix-lab-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/phanix-logo.png',
-  '/favicon.png',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((k) => caches.delete(k)));
+      await self.registration.unregister();
+      const clients = await self.clients.matchAll({ type: "window" });
+      clients.forEach((client) => client.navigate(client.url));
+    })()
   );
 });
