1447a4e4273915bad941cd91e7ccd83d1323c6e6
phaneendharnittala <nittalaphaneendhar@gmail.com>
2026-03-18 19:20:29 +0530

    few changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// Register Service Worker
+// Disable service worker to prevent stale-cache black screens.
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('SW Registered'))
-      .catch(err => console.log('SW Registration Failed', err));
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => Promise.all(registrations.map((r) => r.unregister())))
+      .then(() => console.log('All service workers unregistered'))
+      .catch((err) => console.log('SW unregister failed', err));
   });
 }
 
